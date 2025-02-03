7d82f608ec85def53da445dcf5574ce5cb6bd5e0
Takhlees Fatima <takhleesfatima2003@gmail.com>
2025-02-03 21:24:01 +0500

    commit

--- utils/database.js
+++ utils/database.js
@@ -11,7 +11,7 @@ export const connectToDB = async ()=> {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URL, {
+        await mongoose.connect(process.env.MONGO_URI, {
             dbName: "Mailing_System",
             useNewUrlParser: true,
             useUnifiedTopology: true,
