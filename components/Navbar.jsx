5d05abf95141a8c6c32e36d1ea94fc6f704080be
shariq mehmood <shariqmehmood971@gmail.com>
2025-03-09 12:24:25 +0500

    reminder for mails

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -25,6 +25,7 @@ export default function Navbar() {
   }, [])
 
   return (
+    <div>
     <nav className="bg-[#003559] shadow-md sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
