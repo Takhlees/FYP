41934b06b144c6a8a4f11694a20f6d2d0c1a3543
Takhlees Fatima <takhleesfatima2003@gmail.com>
2025-06-19 18:08:50 +0500

    design3

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
 
   return (
     <>
-      <footer className="bg-deep py-8">
+      <footer className="w-full bg-deep py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center">
             <div>
